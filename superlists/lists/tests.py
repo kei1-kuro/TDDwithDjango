ee170f4a769ae5644609b79ec00cd8038fb7ddf0
kei1 <dev.kei1.kuro@gmail.com>
2019-06-22 21:26:33 +0000

    Refactor home page view to use a template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,4 +15,9 @@ class HomePageTest(TestCase):
         html = response.content.decode('utf8')          
         self.assertTrue(html.startswith('<html>'))          
         self.assertIn('<title>To-Do lists</title>', html)          
-        self.assertTrue(html.endswith('</html>')) 
+        self.assertTrue(html.strip().endswith('</html>'))
+        
+    def test_uses_home_template(self):        
+        response = self.client.get('/')    
+        self.assertTemplateUsed(response, 'home.html')
+        
